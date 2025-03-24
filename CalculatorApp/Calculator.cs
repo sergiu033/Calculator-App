34b130c1e9b0ffffbc1b1560c98a63d115ba99a9
sergiu033 <sergiu.carpinisan.04@gmail.com>
2025-03-24 15:14:28 +0200

    Uploaded the project to github.

---
+++ CalculatorApp/Calculator.cs
@@ -0,0 +1,505 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.CodeDom.Compiler;
+using System.Diagnostics;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Collections.ObjectModel;
+
+namespace CalculatorApp
+{
+    enum CalculatorMode
+    {
+        Standard,
+        Programmer
+    }
+    enum ProgrammerBase
+    {
+        Hex,
+        Dec,
+        Oct,
+        Bin
+    }
+
+    internal class Calculator : INotifyPropertyChanged
+    {
+        private List<string> equationInput;
+        private string equation;
+        private string result;
+        public ObservableCollection<string> memoryStack { get; set; }
+        private bool digitGrouping;
+        private CalculatorMode selectedMode;
+        private ProgrammerBase selectedBase;
+
+        private static string binaryNumberRegex = @"^1[01]*$";
+        private static string octalNumberRegex = @"^[1-7][0-7]*$";
+        private static string hexadecimalNumberRegex = @"^[1-9ABCDEF][0-9ABCDEF]*$";
+        private static string numberRegex = @"^\-?\d+\.?\d*$";
+        private static string operatorRegex = @"^(?:\+|\-|\*|\/|\%|\=)$";
+        private static string functionRegex = @"^(?:sqrt|\^2|1/|\+/\-)$";
+        public string Equation
+        {
+            get { return equation; }
+            set
+            {
+                equation = value;
+                OnPropertyChanged(nameof(Equation));
+            }
+        }
+        public string Result
+        {
+            get { return result; }
+            set
+            {
+                result = value;
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+        public bool DigitGrouping
+        {
+            get { return digitGrouping; }
+            set
+            {
+                digitGrouping = value;
+                OnPropertyChanged(nameof(DigitGrouping));
+            }
+        }
+
+        public CalculatorMode SelectedMode
+        {
+            get { return selectedMode; }
+            set
+            {
+                selectedMode = value;
+                OnPropertyChanged(nameof(SelectedMode));
+            }
+        }
+
+        public ProgrammerBase SelectedBase
+        {
+            get { return selectedBase; }
+            set
+            {
+                selectedBase = value;
+                OnPropertyChanged(nameof(SelectedBase));
+            }
+        }
+
+        public void SetSelectedMode(CalculatorMode mode)
+        {
+            SelectedMode = mode;
+        }
+
+        public void SetProgrammerBase(ProgrammerBase pBase)
+        {
+            SelectedBase = pBase;
+        }
+
+        public Calculator()
+        {
+            equationInput = new List<string>();
+            memoryStack = new ObservableCollection<string>();
+            digitGrouping = false;
+            selectedMode = CalculatorMode.Standard;
+            selectedBase = ProgrammerBase.Dec;
+            equation = "";
+            result = "0";
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string UseDigitGrouping(string input)
+        {
+            string realPart = "";
+            string floatPart = "";
+
+            if (input.Contains("."))
+            {
+                string[] parts = input.Split(new[] { '.' }, 2);
+                realPart = parts[0];
+                floatPart = parts[1];
+            }
+            else
+                realPart = input;
+
+            string newInput = "";
+
+            char[] charArray = realPart.ToCharArray();
+            Array.Reverse(charArray);
+            
+            for (int i = 0; i < realPart.Length; i++)
+            {
+                if (i % 3 == 0)
+                    newInput = ',' + newInput;
+                newInput = charArray[i] + newInput;
+            }
+
+            if (newInput[newInput.Length - 1] == ',')
+                newInput = newInput.Remove(newInput.Length - 1);
+
+            if (!input.Contains("."))
+                return newInput;
+            else
+                return newInput + "." + floatPart;
+        }
+        private void Evaluate()
+        {
+            double lastNumber = 0;
+            string currentOperator = "";
+            bool waitingOperation = false;
+            bool dividedByZero = false;
+            bool squaredRootNegativeNumber = false;
+            foreach (var inp in equationInput)
+            {
+                if (Regex.IsMatch(inp, numberRegex))
+                {
+                    if (!waitingOperation)
+                        lastNumber = Convert.ToDouble(inp);
+                    else if (waitingOperation)
+                    {
+                        if (currentOperator == "+")
+                        {
+                            double temp = lastNumber + Convert.ToDouble(inp);
+                            lastNumber = temp;
+                        }
+                        if (currentOperator == "-")
+                        {
+                            double temp = lastNumber - Convert.ToDouble(inp);
+                            lastNumber = temp;
+                        }
+                        if (currentOperator == "*")
+                        {
+                            double temp = lastNumber * Convert.ToDouble(inp);
+                            lastNumber = temp;
+                        }
+                        if (currentOperator == "/")
+                        {
+                            if (inp == "0")
+                            {
+                                Result = "Cannot divide by zero";
+                                dividedByZero = true;
+                            }
+                            else
+                            {
+                                double temp = lastNumber / Convert.ToDouble(inp);
+                                lastNumber = temp;
+                            }
+                        }
+                        if (currentOperator == "%")
+                        {
+                            double temp = lastNumber % Convert.ToDouble(inp);
+                            lastNumber = temp;
+                        }
+                        waitingOperation = false;
+                    }
+                }
+                else if (Regex.IsMatch(inp, operatorRegex))
+                {
+                    if (inp == "+")
+                        currentOperator = "+";
+                    if (inp == "-")
+                        currentOperator = "-";
+                    if (inp == "*")
+                        currentOperator = "*";
+                    if (inp == "/")
+                        currentOperator = "/";
+                    if (inp == "%")
+                        currentOperator = "%";
+                    waitingOperation = true;
+                }
+                else if (inp == "^2")
+                {
+                    lastNumber *= lastNumber;
+                }
+                else if (inp == "sqrt")
+                {
+                    if (lastNumber < 0)
+                    {
+                        Result = "Cannot sqrt a negative number";
+                        squaredRootNegativeNumber = true;
+                    }
+                    else
+                    {
+                        lastNumber = Math.Sqrt(lastNumber);
+                    }
+                }
+                else if (inp == "1/")
+                {
+                    if (lastNumber == 0)
+                    {
+                        Result = "Cannot divide by zero";
+                        dividedByZero = true;
+                    }
+                    else
+                    {
+                        lastNumber = 1 / lastNumber;
+                    }
+                }
+                else if (inp == "+/-")
+                {
+                    if (lastNumber != 0)
+                        lastNumber = -lastNumber;
+                }
+            }
+            if (!dividedByZero && !squaredRootNegativeNumber)
+            {
+                string finalResult = Convert.ToString(lastNumber);
+                if (!DigitGrouping)
+                    Result = finalResult;
+                else
+                    Result = UseDigitGrouping(finalResult);
+            }
+        }
+
+        private void BuildEquation()
+        {
+            Equation = "";
+            foreach (string input in equationInput)
+            {
+                if (Regex.IsMatch(input, numberRegex))
+                {
+                    if (!DigitGrouping)
+                        Equation += input;
+                    else
+                        Equation += UseDigitGrouping(input);
+                }
+                else if (Regex.IsMatch(input, operatorRegex))
+                {
+                    Equation += input;
+                }
+                else if (Regex.IsMatch(input, functionRegex))
+                {
+                    if (input == "^2")
+                    {
+                        Equation = "(" + Equation + ")^2";
+                    }
+                    if (input == "sqrt")
+                    {
+                        Equation = "sqrt(" + Equation + ")";
+                    }
+                    if (input == "1/")
+                    {
+                        Equation = "1/(" + Equation + ")";
+                    }
+                    if (input == "+/-")
+                    {
+                        Equation = "-(" + Equation + ")";
+                    }
+                }
+            }
+        }
+
+        public void AddInput(string input)
+        {
+            //if the user has started the input
+            if (equationInput.Count != 0)
+            {
+                if (Regex.IsMatch(input, operatorRegex))
+                    Evaluate();
+                if (equationInput.Last() == "=")
+                {
+                    Equation = "";
+                    Result = "0";
+                    equationInput.Clear();
+                }
+                else if (Regex.IsMatch(equationInput.Last(), numberRegex))
+                {
+                    if (input == "." && !equationInput.Last().Contains("."))
+                    {
+                        equationInput[equationInput.Count() - 1] += input;
+                    }
+                    else if (Regex.IsMatch(input, numberRegex))
+                    {
+                        //if last input is 0 we change the 0 into the input (to prevent multiple 0s in a row)
+                        if (equationInput.Last() == "0")
+                        {
+                            equationInput[equationInput.Count() - 1] = input;
+                        }
+                        else
+                        {
+                            equationInput[equationInput.Count() - 1] += input;
+                        }
+                        if (!digitGrouping)
+                            Result = equationInput.Last();
+                        else
+                            Result = UseDigitGrouping(equationInput.Last());
+                    }
+                    else if (Regex.IsMatch(input, operatorRegex))
+                    {
+                        equationInput.Add(input);
+                    }
+                    else if (Regex.IsMatch(input, functionRegex))
+                    {
+                        equationInput.Add(input);
+                    }
+                }
+                else if (Regex.IsMatch(equationInput.Last(), operatorRegex))
+                {
+                    if (Regex.IsMatch(input, numberRegex))
+                    {
+                        equationInput.Add(input);
+                    }
+                    //if current input is an operator we switch it with the current operator
+                    else if (Regex.IsMatch(input, operatorRegex))
+                    {
+                        equationInput[equationInput.Count() - 1] = input;
+                    }
+                    else if (Regex.IsMatch(input, functionRegex))
+                    {
+                        equationInput[equationInput.Count() - 1] = input;
+                        Evaluate();
+                    }
+                }
+                else if (Regex.IsMatch(input, operatorRegex))
+                {
+                    if (Regex.IsMatch(equationInput.Last(), operatorRegex))
+                    {
+                        equationInput.Add(input);
+                        Evaluate();
+                    }
+                }
+                else if (Regex.IsMatch(equationInput.Last(), functionRegex))
+                {
+                    if (Regex.IsMatch(input, functionRegex))
+                    {
+                        if (input == "+/-")
+                        {
+                            if (equationInput.Last() == "+/-")
+                            {
+                                equationInput.Remove(equationInput.Last());
+                            }
+                            else
+                            {
+                                equationInput.Add(input);
+                            }
+                            Evaluate();
+                        }
+                        else
+                        {
+                            equationInput.Add(input);
+                            Evaluate();
+                        }
+                    }
+                }
+            }
+            //if the user hasn't started the input
+            else if (equationInput.Count == 0)
+            {
+                if (Regex.IsMatch(input, numberRegex))
+                {
+                    equationInput.Add(input);
+                }
+            }
+            BuildEquation();
+        }
+
+        public void Backspace()
+        {
+            if (equationInput.Count == 1)
+            {
+                string lastInput = equationInput.Last();
+                if (Regex.IsMatch(lastInput, numberRegex))
+                {
+                    if (lastInput.Length > 1)
+                    {
+                        lastInput = lastInput.Substring(0, lastInput.Length - 1);
+                        equationInput[equationInput.Count - 1] = lastInput;
+                    }
+                    else
+                    {
+                        equationInput[equationInput.Count - 1] = "0";
+                    }
+                }
+            }
+            else if (equationInput.Count > 1)
+            {
+                string lastInput = equationInput.Last();
+                int lastInputIndex = equationInput.Count - 1;
+                if (Regex.IsMatch(lastInput, operatorRegex) || Regex.IsMatch(lastInput, functionRegex))
+                {
+                    equationInput.RemoveAt(lastInputIndex);
+                }
+                else if (Regex.IsMatch(lastInput, numberRegex))
+                {
+                    if (lastInput.Length > 1)
+                    {
+                        lastInput = lastInput.Substring(0, lastInput.Length - 1);
+                        equationInput[equationInput.Count - 1] = lastInput;
+                    }
+                    else
+                    {
+                        equationInput.RemoveAt(lastInputIndex);
+                    }
+                }
+            }
+            BuildEquation();
+            Evaluate();
+        }
+
+        public void Clear()
+        {
+            equationInput.Clear();
+            BuildEquation();
+            Evaluate();
+        }
+
+        public void ClearE()
+        {
+            if (equationInput.Count > 0)
+            {
+                if (Regex.IsMatch(equationInput.Last(), numberRegex))
+                {
+                    equationInput.RemoveAt(equationInput.Count - 1);
+                    Result = "0";
+                }
+            }
+            BuildEquation();
+        }
+
+        public void AddToMemoryStack()
+        {
+            memoryStack.Insert(0, equationInput.Last());
+        }
+
+        public void ClearMemoryStack()
+        {
+            memoryStack.Clear();
+        }
+
+        public void AdditionMemoryStack()
+        {
+            if (memoryStack.Count > 0)
+            {
+                double number = Convert.ToDouble(equationInput.Last()) + Convert.ToDouble(memoryStack[0]);
+                memoryStack[0] = Convert.ToString(number);
+            }
+        }
+
+        public void SubstractMemoryStack()
+        {
+            if (memoryStack.Count > 0)
+            {
+                double number = Convert.ToDouble(memoryStack[0]) - Convert.ToDouble(equationInput.Last());
+                memoryStack[0] = Convert.ToString(number);
+            }
+        }
+
+        public void TopMemoryStack()
+        {
+            if (!digitGrouping)
+                Result = memoryStack[0];
+            else
+                Result = UseDigitGrouping(memoryStack[0]);
+        }
+    }
+}
---
+++ CalculatorApp/MainWindow.xaml.cs
@@ -0,0 +1,224 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculatorApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculator Cal = new Calculator();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = Cal;
+        }
+        private void SetStandardLayout()
+        {
+            Cal.SetSelectedMode(CalculatorMode.Standard);
+            digitGrouping.Visibility = Visibility.Visible;
+            MemoryPad.Visibility = Visibility.Visible;
+            StandardPad.Visibility = Visibility.Visible;
+
+            ProgrammerPad.Visibility = Visibility.Collapsed;
+            BasePad.Visibility = Visibility.Collapsed;
+            ProgrammerBase.Visibility = Visibility.Collapsed;
+        }
+
+        private void SetProgrammerLayout()
+        {
+            Cal.SetSelectedMode(CalculatorMode.Programmer);
+            digitGrouping.Visibility = Visibility.Collapsed;
+            MemoryPad.Visibility = Visibility.Collapsed;
+            StandardPad.Visibility = Visibility.Collapsed;
+
+            ProgrammerPad.Visibility = Visibility.Visible;
+            BasePad.Visibility = Visibility.Visible;
+            ProgrammerBase.Visibility = Visibility.Visible;
+        }
+
+        private void Add_Input_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickButton = sender as Button;
+
+            string content = clickButton.Content as string;
+            Cal.AddInput(content);
+        }
+        private void Backspace_Button_Click(object sender, RoutedEventArgs e) { Cal.Backspace(); }
+        private void Clear_Button_Click(object sender, RoutedEventArgs e) { Cal.Clear(); }
+        private void ClearE_Button_Click(object sender, RoutedEventArgs e) { Cal.ClearE(); }
+        private void Open_Memory_Stack_Button_Click(object sender, RoutedEventArgs e) 
+        {
+            if (StandardPad.Visibility == Visibility.Visible)
+            {
+                StandardPad.Visibility = Visibility.Collapsed;
+                Stack.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                StandardPad.Visibility = Visibility.Visible;
+                Stack.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                // numbers (0-9) and operators %, *
+                case Key.D0:
+                case Key.NumPad0:
+                    Cal.AddInput("0");
+                    break;
+                case Key.D1:
+                case Key.NumPad1:
+                    Cal.AddInput("1");
+                    break;
+                case Key.D2:
+                case Key.NumPad2:
+                    Cal.AddInput("2");
+                    break;
+                case Key.D3:
+                case Key.NumPad3:
+                    Cal.AddInput("3");
+                    break;
+                case Key.D4:
+                case Key.NumPad4:
+                    Cal.AddInput("4");
+                    break;
+                case Key.D5:
+                case Key.NumPad5:
+                    {
+                        if (Keyboard.Modifiers == ModifierKeys.Shift)
+                            Cal.AddInput("%");
+                        else
+                            Cal.AddInput("5");
+                        break;
+                    }
+                case Key.D6:
+                case Key.NumPad6:
+                    Cal.AddInput("6");
+                    break;
+                case Key.D7:
+                case Key.NumPad7:
+                    Cal.AddInput("7");
+                    break;
+                case Key.D8:
+                case Key.NumPad8:
+                    {
+                        if (Keyboard.Modifiers == ModifierKeys.Shift)
+                            Cal.AddInput("*");
+                        else
+                            Cal.AddInput("8");
+                        break;
+                    }
+                case Key.D9:
+                case Key.NumPad9:
+                    Cal.AddInput("9");
+                    break;
+
+                // operators (+, -, /, *)
+                case Key.Add:
+                case Key.OemPlus:
+                    Cal.AddInput("+");
+                    break;
+                case Key.Subtract:
+                case Key.OemMinus:
+                    Cal.AddInput("-");
+                    break;
+                case Key.Divide:
+                case Key.OemQuestion:
+                    Cal.AddInput("/");
+                    break;
+                case Key.Multiply:
+                    Cal.AddInput("*");
+                    break;
+
+                // functions (^2, sqrt, 1/)
+                case Key.S:
+                    Cal.AddInput("^2");
+                    break;
+                case Key.R:
+                    Cal.AddInput("sqrt");
+                    break;
+                case Key.D:
+                    Cal.AddInput("1/");
+                    break;
+
+                // = operator and backspace
+                case Key.Enter:
+                    Cal.AddInput("=");
+                    break;
+                case Key.Back:
+                    Cal.Backspace();
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void Store_In_Memory_Stack_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Cal.AddToMemoryStack();
+        }
+
+        private void Clear_Memory_Stack_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Cal.ClearMemoryStack();
+        }
+
+        private void Add_Memory_Stack_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Cal.AdditionMemoryStack();
+        }
+
+        private void Substract_Memory_Stack_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Cal.SubstractMemoryStack();
+        }
+
+        private void Display_Top_Memory_Stack_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Cal.TopMemoryStack();
+        }
+
+        private void Open_Settings_Click(object sender, MouseButtonEventArgs e)
+        {
+            SettingsPopUp.IsOpen = !SettingsPopUp.IsOpen;
+        }
+
+        private void Set_Mode_Click(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            string mode = clickedButton.Content as string;
+            if (mode == "Standard")
+                SetStandardLayout();
+            else if (mode == "Programmer")
+                SetProgrammerLayout();
+        }
+
+        private void Set_Programmer_Base(object sender, RoutedEventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            string programmerBase = clickedButton.Content as string;
+            if (programmerBase == "Bin")
+                Cal.SetProgrammerBase(CalculatorApp.ProgrammerBase.Bin);
+            else if (programmerBase == "Oct")
+                Cal.SetProgrammerBase(CalculatorApp.ProgrammerBase.Oct);
+            else if (programmerBase == "Dec")
+                Cal.SetProgrammerBase(CalculatorApp.ProgrammerBase.Dec);
+            else if (programmerBase == "Hex")
+                Cal.SetProgrammerBase(CalculatorApp.ProgrammerBase.Hex);
+        }
+    }
+}
